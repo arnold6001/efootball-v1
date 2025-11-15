5dac6ab48181fec7d3b6b214ae3bb232a9187812
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-11-15 18:12:06 +0300

    Create src/app/dashboard/page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+// ... add
+<Link href="/profile" className="btn btn-info mr-4">Profile</Link>
+<Link href="/leaderboard" className="btn btn-success">Leaderboard</Link>
+<Link href="/calendar" className="btn btn-warning">Event Calendar</Link>
