90318bf79b6bb954079487aafa4a700879f4ea02
Arnold Chirchir <arnoldkipruto193@gmail.com>
2025-11-15 18:34:41 +0300

    Delete src directory

--- src/app/layout.tsx
+++
@@ -1,2 +0,0 @@
-// Same as before, but add data-theme="cyberpunk" to body for gaming feel.
-<body className={`${inter.className}`} data-theme="cyberpunk">
--- src/app/tournaments/create/page.tsx
+++
@@ -1,9 +0,0 @@
-// ... add fields
-const [format, setFormat] = useState('single-elim'); // or 'round-robin'
-const [teamBased, setTeamBased] = useState(false);
-const [prize, setPrize] = useState('');
-
-// In tournament object:
-format,
-teamBased,
-prize,
